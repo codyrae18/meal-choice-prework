fabfdb235b10fa474bb6fae05f669985c808a5f6
Cody Lawdermilt <lawdermiltcody@gmail.com>
2019-07-11 05:50:09 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,7 +1,5 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
-def meal_choice(food = "meat")
-  if 
-    
-  else food
+def meal_choice(arg1)
+  
 end
